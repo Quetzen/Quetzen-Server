462d779f4b5b4cdbd9a931c554dec066a05081c1
Quetzen <Quetzen_militech@hotmail.com>
2022-04-02 14:49:07 -0400

    Working on it!

---
+++ assets/tiles/Digi DD teleports.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Digi DD teleports" tilewidth="64" tileheight="32" tilecount="12" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="32"/>
+ <image source="Digi DD teleports.PNG" width="256" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="24">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="3" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="24">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="4" duration="120"/>
+   <frame tileid="5" duration="120"/>
+   <frame tileid="6" duration="120"/>
+   <frame tileid="7" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="24">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="8" duration="120"/>
+   <frame tileid="9" duration="120"/>
+   <frame tileid="10" duration="120"/>
+   <frame tileid="11" duration="120"/>
+  </animation>
+ </tile>
+</tileset>
