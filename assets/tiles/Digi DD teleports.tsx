e64a7e0d6252ecaa8f056d25c630d75e2efcfa29
ninjaman255 <aaronclevette@gmail.com>
2022-04-05 01:41:21 -0500

    Fixed HP Warp for real this time

--- assets/tiles/Digi DD teleports.tsx
+++ assets/tiles/Digi DD teleports.tsx
@@ -4,8 +4,8 @@
  <image source="Digi DD teleports.png" width="256" height="96"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="30" y="4">
-    <polygon points="0,0 4,0 20,8 20,11 4,19 0,19 -16,11 -16,8"/>
+   <object id="1" x="30.2222" y="7">
+    <polygon points="0,0 2.55556,0 12.7778,5.05263 12.7778,6.94737 2.55556,12 0,12 -10.2222,6.94737 -10.2222,5.05263"/>
    </object>
   </objectgroup>
   <animation>
