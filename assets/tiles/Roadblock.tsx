eed2c2dc6d5fee8c355cac07e71fe6a7c08c8fcf
Quetzen <Quetzen_militech@hotmail.com>
2022-04-06 08:57:09 -0400

    Minor tweaks
    Cropped fountain, fixed road block hitboxes

--- assets/tiles/Roadblock.tsx
+++ assets/tiles/Roadblock.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Roadblock" tilewidth="64" tileheight="40" tilecount="1" columns="1" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.8.2" name="Roadblock" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="16"/>
- <image source="Roadblock.png" width="64" height="40"/>
+ <image source="Roadblock.png" width="64" height="64"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-16" y="20">
-    <polygon points="0,0 16,-10 80,20 64,30"/>
+   <object id="2" x="32" y="64">
+    <polygon points="0,0 7.936,-3.968 -23.808,-19.84 -31.744,-15.872"/>
    </object>
   </objectgroup>
  </tile>
