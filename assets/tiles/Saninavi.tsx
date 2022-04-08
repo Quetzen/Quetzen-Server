7a8ff3a2e0b300bd0a2ae3f3225e4b933f826082
Quetzen <Quetzen_militech@hotmail.com>
2022-04-08 09:12:55 -0400

    Some junk added
    Ready for upload. New build, repurposing of the two squares. Added a few more placeholder roads, removed useless ones. Need plan for the Big Street. Ready for NPCs.

---
+++ assets/tiles/Saninavi.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Saninavi" tilewidth="64" tileheight="40" tilecount="2" columns="2">
+ <tileoffset x="0" y="16"/>
+ <image source="Saninavi.png" width="128" height="40"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="32" y="40">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="40">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
