257afa07cf94f6274f26e336585282c0fd3ff6ec
Quetzen <Quetzen_militech@hotmail.com>
2022-04-07 20:20:18 -0400

    Roadblocks
    Turned to squares.

--- assets/tiles/Roadblock.tsx
+++ assets/tiles/Roadblock.tsx
@@ -1,11 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Roadblock" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="8"/>
- <image source="Roadblock.png" width="64" height="64"/>
+<tileset version="1.8" tiledversion="1.8.2" name="Roadblock" tilewidth="64" tileheight="64" tilecount="2" columns="2" objectalignment="bottom">
+ <tileoffset x="0" y="16"/>
+ <image source="Roadblock.png" width="128" height="64"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="32" y="64">
-    <polygon points="0,0 7.936,-3.968 -23.808,-19.84 -31.744,-15.872"/>
+   <object id="1" x="32" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
    </object>
   </objectgroup>
  </tile>
