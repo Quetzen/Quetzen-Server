40cb0ad11320f26fa31e6396748f0c000da51227
Quetzen <Quetzen_militech@hotmail.com>
2022-04-09 02:04:42 -0400

    NPCs
    SO MANY NPCS

--- assets/tiles/obstacle stump.tsx
+++ assets/tiles/obstacle stump.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="obstacle stump" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+<tileset version="1.8" tiledversion="1.8.2" name="obstacle stump" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="16"/>
  <image source="obstacle stump.png" width="64" height="64"/>
  <tile id="0">
