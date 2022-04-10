7b31619e6bdf19041b6c9df14a6e2b3c72385f06
Quetzen <Quetzen_militech@hotmail.com>
2022-04-10 10:49:34 -0400

    New assets
    Added more NPCs where there were empty spaces. More empty spaces need filling, so added more props, will add more later. Update new batch.

---
+++ assets/tiles/Digimon farm pylon.tsx
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Digimon farm pylon" tilewidth="32" tileheight="64" tilecount="9" columns="3" objectalignment="bottom">
+ <tileoffset x="0" y="16"/>
+ <image source="Digimon farm pylon.png" width="96" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="160"/>
+   <frame tileid="1" duration="160"/>
+   <frame tileid="2" duration="160"/>
+   <frame tileid="1" duration="160"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="3" duration="160"/>
+   <frame tileid="4" duration="160"/>
+   <frame tileid="5" duration="160"/>
+   <frame tileid="4" duration="160"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 16,-8 0,-16 -16,-8"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="6" duration="160"/>
+   <frame tileid="7" duration="160"/>
+   <frame tileid="8" duration="160"/>
+   <frame tileid="7" duration="160"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/tiles/Preview holograms.tsx
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Preview holograms" tilewidth="64" tileheight="128" tilecount="10" columns="5">
+ <tileoffset x="0" y="16"/>
+ <image source="Preview holograms.png" width="320" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="2" duration="1600"/>
+   <frame tileid="3" duration="1600"/>
+   <frame tileid="4" duration="1600"/>
+   <frame tileid="5" duration="1600"/>
+   <frame tileid="6" duration="1600"/>
+   <frame tileid="7" duration="1600"/>
+   <frame tileid="8" duration="1600"/>
+   <frame tileid="9" duration="1600"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
