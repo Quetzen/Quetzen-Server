3f724069b674e399c3c1b30b0a87dbdc4e311232
Quetzen <Quetzen_militech@hotmail.com>
2022-04-10 11:21:06 -0400

    ...slight oopsie fixed

--- assets/tiles/DigimonWorldGatewall.tsx
+++ assets/tiles/DigimonWorldGatewall.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="DigimonWorldGatewall" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.8.2" name="DigimonWorldGatewall" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="32"/>
- <image source="DigimonWorldGatewall.PNG" width="64" height="64"/>
+ <image source="DigimonWorldGatewall.png" width="64" height="64"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="32" y="48">
