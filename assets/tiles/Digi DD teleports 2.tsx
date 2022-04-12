3f7c9ad6d509312c71d7023c671e22a021d32527
Quetzen <Quetzen_militech@hotmail.com>
2022-04-12 16:39:19 -0400

    Lots of polish
    Fixed a wall placement. Fixed a break in the tileset art. Added several new props. Removed Herobrine.

---
+++ assets/tiles/Digi DD teleports 2.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Digi DD teleports 2" tilewidth="64" tileheight="32" tilecount="4" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="32"/>
+ <image source="Digi DD teleports 2.png" width="256" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="3" duration="120"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/tiles/obstacle stone.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="obstacle stone" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="obstacle stone.png" width="192" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="64">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
