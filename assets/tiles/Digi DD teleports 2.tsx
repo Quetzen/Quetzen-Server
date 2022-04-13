bac2ae3809c7b1c656bae7199cc458df156a5810
ninjaman255 <aaronclevette@gmail.com>
2022-04-12 19:10:09 -0500

    Maybe Fixed the warp

--- assets/tiles/Digi DD teleports 2.tsx
+++ assets/tiles/Digi DD teleports 2.tsx
@@ -1,8 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Digi DD teleports 2" tilewidth="64" tileheight="32" tilecount="4" columns="4" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.8.4" name="Digi DD teleports 2" tilewidth="64" tileheight="32" tilecount="4" columns="4" objectalignment="bottom">
  <tileoffset x="0" y="32"/>
  <image source="Digi DD teleports 2.png" width="256" height="32"/>
  <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="20" y="9" width="24" height="14">
+    <ellipse/>
+   </object>
+  </objectgroup>
   <animation>
    <frame tileid="0" duration="120"/>
    <frame tileid="1" duration="120"/>
