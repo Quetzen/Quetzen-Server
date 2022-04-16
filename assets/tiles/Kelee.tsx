16fae2c5c90dedb17f3922e45d7530b250b2d10f
Quetzen <Quetzen_militech@hotmail.com>
2022-04-15 20:22:58 -0400

    Changes aplenty

--- assets/tiles/Kelee.tsx
+++ assets/tiles/Kelee.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Kelee" tilewidth="32" tileheight="64" tilecount="1" columns="1">
+<tileset version="1.8" tiledversion="1.8.2" name="Kelee" tilewidth="32" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="24"/>
  <image source="Kelee.png" width="32" height="64"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
---
+++ assets/tiles/Lex but in water.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Lex but in water" tilewidth="21" tileheight="31" tilecount="1" columns="1">
+ <image source="Lex but in water.png" width="21" height="31"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="10" y="31">
+    <polygon points="0,0 8,-4 0,-8 -8,-4"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
