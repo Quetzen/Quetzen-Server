d455f6c0db7caf855113d51cdd635674c74ac066
Quetzen <Quetzen_militech@hotmail.com>
2022-04-15 21:03:21 -0400

    Dusted off more stuff. Ready for re-update.

--- assets/tiles/Lex but in water.tsx
+++ assets/tiles/Lex but in water.tsx
@@ -2,9 +2,9 @@
 <tileset version="1.8" tiledversion="1.8.2" name="Lex but in water" tilewidth="21" tileheight="31" tilecount="1" columns="1">
  <image source="Lex but in water.png" width="21" height="31"/>
  <tile id="0">
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="10" y="31">
-    <polygon points="0,0 8,-4 0,-8 -8,-4"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="42" y="23.25">
+    <polygon points="0,0 -32,16 -64,0 -32,-16"/>
    </object>
   </objectgroup>
  </tile>
