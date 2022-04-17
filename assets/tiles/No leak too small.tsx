6fc140e4524e5ef9c6674cbb9182a8e6efe23e97
Quetzen <Quetzen_militech@hotmail.com>
2022-04-16 21:47:51 -0400

    Redesigns
    Added doors and new paths. Cleaned 2 of the squares up. Deleted Herobrine.

---
+++ assets/tiles/No leak too small.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="No leak too small" tilewidth="64" tileheight="48" tilecount="4" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="48"/>
+ <image source="No leak too small.png" width="256" height="48"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="32" y="32">
+    <polygon points="0,0 32,-16 0,-32 -32,-16"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="3" duration="120"/>
+  </animation>
+ </tile>
+</tileset>
