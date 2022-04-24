cc28c395ed566a56ecd9a5f538142860a89c8b59
Quetzen <Quetzen_militech@hotmail.com>
2022-04-23 23:35:53 -0400

    Changed all str to int
    Strength to Intellect?

---
+++ assets/tiles/Net doors 2.tsx
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Net doors 2" tilewidth="32" tileheight="128" tilecount="12" columns="6">
+ <image source="Net doors 2.png" width="192" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 -32,-16 -24,-20 8,-4"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="60"/>
+   <frame tileid="1" duration="60"/>
+   <frame tileid="2" duration="60"/>
+   <frame tileid="1" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="112">
+    <polygon points="0,0 -32,-16 -24,-20 8,-4"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="3" duration="60"/>
+   <frame tileid="4" duration="60"/>
+   <frame tileid="5" duration="60"/>
+   <frame tileid="4" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="128">
+    <polygon points="0,0 -32,-16 -24,-20 8,-4"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="6" duration="60"/>
+   <frame tileid="7" duration="60"/>
+   <frame tileid="8" duration="60"/>
+   <frame tileid="7" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="112">
+    <polygon points="0,0 -32,-16 -24,-20 8,-4"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="9" duration="60"/>
+   <frame tileid="10" duration="60"/>
+   <frame tileid="11" duration="60"/>
+   <frame tileid="10" duration="60"/>
+  </animation>
+ </tile>
+</tileset>
