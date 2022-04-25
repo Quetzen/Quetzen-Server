6e2123801eca8480db8ca2d079c23b770734651e
Quetzen <Quetzen_militech@hotmail.com>
2022-04-25 18:08:24 -0400

    Added more stuff to area2
    Need teleport sprites.

---
+++ assets/tiles/Artificial sun.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Artificial sun" tilewidth="128" tileheight="96" tilecount="9" columns="3">
+ <tileoffset x="0" y="32"/>
+ <image source="Artificial sun.png" width="384" height="288"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="64" y="96">
+    <polygon points="0,0 -64,-32 0,-64 64,-32"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="1" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="64" y="96">
+    <polygon points="0,0 -64,-32 0,-64 64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="250"/>
+   <frame tileid="7" duration="250"/>
+   <frame tileid="8" duration="250"/>
+   <frame tileid="7" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/tiles/Flat Terminal.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Flat Tileset" tilewidth="64" tileheight="39" tilecount="6" columns="2">
+ <tileoffset x="0" y="7"/>
+ <image source="Flat terminal.PNG" trans="ffffff" width="128" height="117"/>
+</tileset>
