e4988ec33bb3c340b3ff049a6bb11a725372c8ce
Quetzen <Quetzen_militech@hotmail.com>
2022-05-08 06:34:17 -0400

    More update.

---
+++ assets/tiles/vendmach.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="vendmach" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+ <tileoffset x="0" y="32"/>
+ <image source="vendmach.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="128" y="96">
+    <polygon points="0,0 -64,32 -96,16 -32,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/tiles/vendmach2.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="vendmach2" tilewidth="96" tileheight="96" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="24"/>
+ <image source="vendmach2.png" width="96" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="48" y="96">
+    <polygon points="0,0 -48.0219,-24.2963 -24.011,-36.4444 24.011,-12.1481"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
