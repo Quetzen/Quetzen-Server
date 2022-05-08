7e56d066966916d85e31ca4c5c1496369ff52069
Quetzen <Quetzen_militech@hotmail.com>
2022-05-08 04:38:24 -0400

    Indus pump!
    Changed some plaza area stuff, and added a ton in indus.

---
+++ assets/tiles/Cyberworld Opennet.tsx
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Cyberworld Opennet" tilewidth="64" tileheight="64" tilecount="48" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="16"/>
+ <image source="Cyberworld Opennet.png" width="256" height="768"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="32" duration="250"/>
+   <frame tileid="36" duration="250"/>
+   <frame tileid="40" duration="250"/>
+   <frame tileid="36" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="33" duration="250"/>
+   <frame tileid="37" duration="250"/>
+   <frame tileid="41" duration="250"/>
+   <frame tileid="37" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="34" duration="250"/>
+   <frame tileid="38" duration="250"/>
+   <frame tileid="42" duration="250"/>
+   <frame tileid="38" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="35" duration="250"/>
+   <frame tileid="39" duration="250"/>
+   <frame tileid="43" duration="250"/>
+   <frame tileid="39" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0,0 -32,16 0,32 32,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
