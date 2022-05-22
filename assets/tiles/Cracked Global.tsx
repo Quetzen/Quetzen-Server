4dee1f0f07ced82c05fcc396512cd9622018c552
Quetzen <Quetzen_militech@hotmail.com>
2022-05-21 20:01:09 -0400

    Ready for update

---
+++ assets/tiles/Cracked Global.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Cracked Global" tilewidth="64" tileheight="39" tilecount="6" columns="2">
+ <tileoffset x="0" y="7"/>
+ <image source="Cracked Global.png" width="128" height="117"/>
+</tileset>
