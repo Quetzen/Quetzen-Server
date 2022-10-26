e1b06ddf79f4738ee044674e1179d1413b9c77d1
Quetzen <Quetzen_militech@hotmail.com>
2022-10-25 22:07:19 -0400

    LOTS CHANGE MANY THINGS
    Shrunk one of the areas until it worked. Let's goooo.

--- assets/tiles/DigimonWorldDoodad2.tsx
+++ assets/tiles/DigimonWorldDoodad2.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="DigimonWorldDoodad2" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="../../../../MY resources/DigimonWorldDoodad2.PNG" trans="ffffff" width="96" height="32"/>
+<tileset version="1.8" tiledversion="1.8.5" name="DigimonWorldDoodad2" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="DigimonWorldDoodad2.png" trans="ffffff" width="96" height="32"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="16" y="32">
---
+++ assets/tiles/netcity tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="netcity tileset" tilewidth="64" tileheight="40" tilecount="4" columns="2">
+ <tileoffset x="0" y="8"/>
+ <image source="netcity tileset.png" width="128" height="80"/>
+</tileset>
