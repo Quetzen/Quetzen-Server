c8403e57181e06348dcaf416d2427562088b2c8f
Quetzen <Quetzen_militech@hotmail.com>
2022-12-08 19:41:44 -0500

    Just some tiles
    Added 2 tilesets, Arena data flow push cancelled. Will look funky, but I know why.

---
+++ assets/tiles/Undernet test.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Undernet test" tilewidth="70" tileheight="40" tilecount="1" columns="1">
+ <tileoffset x="-3" y="8"/>
+ <image source="../../../../../Desktop/games/Open Netbattle/Release/resources/ow/tiles/Undernet test.png" width="70" height="40"/>
+</tileset>
